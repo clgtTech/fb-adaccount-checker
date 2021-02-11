956a092f5cd4578f802e7458c22453a95b01de26
Alexander Zimakov <alexzimakov@icloud.com>
2021-02-11 22:35:59 +0700

    Migrate saved users from v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import './migrate-saved-users';
 import './index.scss';
 import { AppRoot } from './app-root';
 
